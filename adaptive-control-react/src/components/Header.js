c046b733c2473830b10ea760b52ea15f537f296d
satej soman <satej.soman@gmail.com>
2020-12-02 23:58:36 -0600

    update header

--- adaptive-control-react/src/components/Header.js
+++ adaptive-control-react/src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => <>
     <Card className="text-center" style={{"background-color": "#343a40", "opacity": 0.95}}> 
     <Card.Body>
     <h1 className="text-white" style={{"background-color": "#343a40", "opacity": 0.95}}>
-      {" "}Adaptive Control of COVID-19 in India{" "}
+      {" "}Adaptive Control of COVID-19 in Indonesia{" "}
     </h1>
     <Card.Subtitle as="h3" className="text-light">
         {" "}
@@ -13,7 +13,7 @@ const Header = () => <>
       </Card.Subtitle>
       <hr />
       <div className="bg-light rounded">
-      <strong>Disclaimer</strong>: Our estimators are based on data from <a href="https://www.covid19india.org">Covid19India</a>, a crowd-sourced database of infection information for India. The estimators and visualizations on this website are an active work in progress and subject to revision as we improve their accuracy. If you have comments or corrections, please <a href="https://github.com/mansueto-institute/adaptive-control-feedback/issues/new/choose">file an issue on GitHub</a>.
+      <strong>Disclaimer</strong>: Our estimators are based on data from <a href="https://covid19.go.id/peta-sebaran">the Indonesian government's official coronavirus tracker</a>. The estimators and visualizations on this website are an active work in progress and subject to revision as we improve their accuracy. If you have comments or corrections, please <a href="https://github.com/mansueto-institute/adaptive-control-feedback/issues/new/choose">file an issue on GitHub</a>.
       </div>
     </Card.Body>
   </Card>
